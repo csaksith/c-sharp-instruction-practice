3337d30916f011882444155a3af538db1f463305
csaksith <saksitca@mail.uc.edu>
2025-03-22 17:04:10 -0400

    git init

---
+++ 02RectangleCalculator/02RectangleCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleLibrary;
+
+namespace _02RectangleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("Welcome to the Area and Perimeter Calculator!~");
+
+        }
+    }
+}
