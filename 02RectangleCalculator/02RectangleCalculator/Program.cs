4766a741c631d59f59b627a809dd35e01e097aec
csaksith <saksitca@mail.uc.edu>
2025-03-22 17:30:16 -0400

    complete 2-3 rectangle calculator

--- 02RectangleCalculator/02RectangleCalculator/Program.cs
+++ 02RectangleCalculator/02RectangleCalculator/Program.cs
@@ -6,8 +6,18 @@ namespace _02RectangleCalculator
     {
         static void Main(string[] args)
         {
-            MyConsole.PrintLine("Welcome to the Area and Perimeter Calculator!~");
-
+            MyConsole.PrintLine("Welcome to the Area and Perimeter Calculator!~\n");
+            string choice = "y";
+            while (choice =="y") {
+                double length = MyConsole.PromptDouble("Enter length: ");
+                double width = MyConsole.PromptDouble("Enter width: ");
+                double area = length*width;
+                double perimeter = (length*2)+(width*2);
+                MyConsole.PrintLine($"Area: {area.ToString("F1")}");
+                MyConsole.PrintLine($"Perimeter: {perimeter.ToString("F1")}");
+                choice=MyConsole.PromptReqString("\nContinue? (y/n): ","y","n");
+            }
+            MyConsole.PrintLine("\nBye!~");
         }
     }
 }
