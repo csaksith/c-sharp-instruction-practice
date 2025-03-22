b0df50cffe7576564fa3c4eaf2d2205479e7666c
csaksith <saksitca@mail.uc.edu>
2025-03-22 18:39:22 -0400

    complete 3-3 interest calculator

--- 03InterestCalculator/03InterestCalculator/Program.cs
+++ 03InterestCalculator/03InterestCalculator/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace _03InterestCalculator
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+﻿using ConsoleLibrary;
+
+namespace _03InterestCalculator {
+    internal class Program {
+        static void Main(string[] args) {
+            MyConsole.PrintLine("Welcome to the Interest Calculator!~\n");
+            string choice = "y";
+            while (choice.ToLower()=="y") {
+                decimal loanAmt = MyConsole.PromptDecimal("Enter loan amount: ");
+                decimal interestRate = MyConsole.PromptDecimal("Enter interest rate: ");
+                MyConsole.PrintLine($"\nLoan amount: {loanAmt:C}");
+                MyConsole.PrintLine($"Interest rate: {interestRate:P3}");
+                decimal interest = loanAmt*interestRate;
+                MyConsole.PrintLine($"Interest: {interest:C}");
+                choice=MyConsole.PromptReqString("\nContinue? (y/n): ","y","n").ToLower();
+            }
+            MyConsole.PrintLine("Bye~");
         }
     }
 }
