78f6f5eda936b00c91f294ab93ccc06684ce933d
csaksith <saksitca@mail.uc.edu>
2025-03-22 18:00:56 -0400

    git init

--- 03TemperatureConverter/03TemperatureConverter/Program.cs
+++ 03TemperatureConverter/03TemperatureConverter/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace _03TemperatureConverter
+﻿using ConsoleLibrary;
+
+namespace _03TemperatureConverter
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MyConsole.PrintLine("Welcome to the Temperature Converter!");
+            string choice = "y";
+            while (choice.ToLower()=="y") {
+                double degF = MyConsole.PromptDouble("\nEnter degrees in Fahrenheit: ");
+                double degC = (degF-32)*5/9;
+                MyConsole.PrintLine($"Degrees in Celsius: {degC:F2}");
+                choice = MyConsole.PromptReqString("\nContinue? (y/n): ","y","n");
+            }
+            MyConsole.PrintLine("Goodbye!");
         }
     }
 }
---
+++ 03TravelTimeCalculator/03TravelTimeCalculator/Program.cs
@@ -0,0 +1,9 @@
+﻿using ConsoleLibrary;
+
+namespace _03TravelTimeCalculator {
+    internal class Program {
+        static void Main(string[] args) {
+            MyConsole.PrintLine("Welcome to the Travel Time Calculator!~");
+        }
+    }
+}
