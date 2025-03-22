598959a55c43011721318913bffec64b8b0cfef3
csaksith <saksitca@mail.uc.edu>
2025-03-22 18:30:15 -0400

    complete travel time calculator

--- 03TravelTimeCalculator/03TravelTimeCalculator/Program.cs
+++ 03TravelTimeCalculator/03TravelTimeCalculator/Program.cs
@@ -3,7 +3,23 @@
 namespace _03TravelTimeCalculator {
     internal class Program {
         static void Main(string[] args) {
-            MyConsole.PrintLine("Welcome to the Travel Time Calculator!~");
+            MyConsole.PrintLine("Welcome to the Travel Time Calculator!~\n");
+            string choice = "y";
+            while (choice=="y") {
+                double miles = MyConsole.PromptDouble("Enter miles: ");
+                double mph = MyConsole.PromptDouble("Enter miles per hour: ");
+                // Calculate time in hours
+                // hours = miles /mph
+                double totalHours = (miles/mph);
+                int hours = (int)totalHours;
+                double remainder = totalHours%1;
+                int minutes = (int)(remainder*60);
+                MyConsole.PrintLine("\nEstimated travel time");
+                MyConsole.PrintLine("---------------------");
+                MyConsole.PrintLine($"Hours: {hours}");
+                MyConsole.PrintLine($"Minutes: {minutes}");
+                choice = MyConsole.PromptReqString("Continue? (y/n): ","y","n");
+            }
         }
     }
 }
