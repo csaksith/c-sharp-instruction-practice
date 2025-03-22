241205ce941d2cf7f9ee65f69a5ab77baf3a3542
csaksith <saksitca@mail.uc.edu>
2025-03-22 15:03:08 -0400

    complete 2-2 grade converter

---
+++ 02GradeConverter/02GradeConverter/Program.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleLibrary;
+
+namespace _02GradeConverter {
+    internal class Program {
+        static void Main(string[] args) {
+            MyConsole.PrintLine("Welcome to the Letter Grade Converter~");
+
+            string choice = "y";
+            string letterGrade = "F";
+            while (choice=="y") {
+                int numGrade = MyConsole.PromptInt("\nEnter numerical grade: ");
+
+                if (numGrade>88&&numGrade<100) {
+                    letterGrade="A";
+                    MyConsole.PrintLine($"Letter Grade: {letterGrade}");
+                }
+                else if (numGrade>=80&&numGrade<=87) {
+                    letterGrade="B";
+                    MyConsole.PrintLine($"Letter Grade: {letterGrade}");
+                }
+                else if (numGrade>=68&&numGrade<=79) {
+                    letterGrade="C";
+                    MyConsole.PrintLine($"Letter Grade: {letterGrade}");
+                }
+                else if (numGrade>=60&&numGrade<=67) {
+                    letterGrade="D";
+                    MyConsole.PrintLine($"Letter Grade: {letterGrade}");
+                }
+                else {
+                    letterGrade="F";
+                    MyConsole.PrintLine($"Letter Grade: {letterGrade}");
+                }
+                choice=MyConsole.PromptReqString("\nContinue? (y/n): ","y","n");
+            }
+            MyConsole.PrintLine("\nGoodbye!");
+        }
+    }
+}
