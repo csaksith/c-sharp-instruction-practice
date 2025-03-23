d8f927f302adf5ccdff0e56eb7c6011b049904d6
csaksith <saksitca@mail.uc.edu>
2025-03-23 16:51:43 -0400

    complete 4-4 common divisor calculator

---
+++ 04CommonDivisor/04CommonDivisor/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleLibrary;
+
+namespace _04CommonDivisor {
+    internal class Program {
+        static void Main(string[] args) {
+            MyConsole.PrintLine("Welcome to the Greatest Common Divisor!~");
+            string choice = "y";
+            while (choice.ToLower()=="y") {
+                int x = MyConsole.PromptInt("\nEnter first number: ");
+                int y = MyConsole.PromptInt("Enter second number: ");
+                while (x!=0) {
+                    while (y>=x) {
+                        y-=x;
+                    }
+                    int tempY = y;
+                    y=x; // swap x and y
+                    x=tempY; // set x to the previous value of y
+                }
+                MyConsole.PrintLine($"Greatest common divisor: {y}");
+                choice=MyConsole.PromptReqString("\nContinue? (y/n): ","y","n");
+            }
+            MyConsole.PrintLine("\nGoodbye!~");
+        }
+    }
+}
