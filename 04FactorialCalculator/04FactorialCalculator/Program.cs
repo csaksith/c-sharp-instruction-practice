c38da88b67a9da7bb856a0e38fd4b17c298cde31
csaksith <saksitca@mail.uc.edu>
2025-03-22 21:22:42 -0400

    git init

---
+++ 04FactorialCalculator/04FactorialCalculator/Program.cs
@@ -0,0 +1,9 @@
+﻿using ConsoleLibrary;
+
+namespace _04FactorialCalculator {
+    internal class Program {
+        static void Main(string[] args) {
+            MyConsole.PrintLine("Welcome to the Factorial Calculator!~");
+        }
+    }
+}
