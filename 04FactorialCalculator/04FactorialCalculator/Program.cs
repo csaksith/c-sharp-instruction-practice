0ea499764603fb8d47d8c25b7842a0e83a14fc2b
csaksith <saksitca@mail.uc.edu>
2025-03-22 21:44:05 -0400

    complete 4-2 factorial calculator

--- 04FactorialCalculator/04FactorialCalculator/Program.cs
+++ 04FactorialCalculator/04FactorialCalculator/Program.cs
@@ -4,6 +4,17 @@ namespace _04FactorialCalculator {
     internal class Program {
         static void Main(string[] args) {
             MyConsole.PrintLine("Welcome to the Factorial Calculator!~");
+            string choice = "y";
+            while (choice=="y") {
+                int factorial = 1;
+                int integer = MyConsole.PromptInt("\nEnter an integer that's greater than 0 and less than 10: ");
+                for (int i = 1; i<=integer; i++) {
+                    factorial*=i;
+                }
+                MyConsole.PrintLine($"The factorial of {integer} is {factorial}.\n");
+                choice=MyConsole.PromptReqString("Continue? (y/n): ","y","n");
+            }
+            MyConsole.PrintLine("\nBye!~");
         }
     }
 }
