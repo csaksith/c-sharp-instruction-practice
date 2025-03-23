620ab2e3b7e326e633245528bf088836db7d4f0c
csaksith <saksitca@mail.uc.edu>
2025-03-23 11:30:46 -0400

    git init

---
+++ 04TipCalculator/04TipCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleLibrary;
+
+namespace _04TipCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("Welcome to the Tip Calculator!~");
+        }
+    }
+}
