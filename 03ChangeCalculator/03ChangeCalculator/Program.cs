50cde21e367273f4332b3d70f38867f2147548fa
csaksith <saksitca@mail.uc.edu>
2025-03-22 20:54:36 -0400

    complete 3-4 change calculator

---
+++ 03ChangeCalculator/03ChangeCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleLibrary;
+
+namespace _03ChangeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("Welcome to the Change Calculator!");
+            int cents = MyConsole.PromptInt("Enter # of cents (0-99): \n");
+            int quarters = cents/25;
+            int dimes = (cents%25)/10;
+            int nickels = (cents%25%10)/5;
+            int pennies = (cents%25%10%5);
+            MyConsole.PrintLine($"Quarters: {quarters}\nDimes: {dimes} \nNickels: {nickels} \nPennies: {pennies} \n");
+
+
+
+
+        }
+    }
+}
